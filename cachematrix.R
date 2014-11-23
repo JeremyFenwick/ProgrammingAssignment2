916ef408a4609f0f125329915d7e49cd4a67e230
JeremyFenwick <jeremy.fenwick@gmail.com>
2014-11-23 17:20:01 +1100

    My Code
    Replaced the supplied R file with my code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) { # Create the function makeCacheMatrix
+  s <- NULL # "s" is the result of solve(), set to NULL. This way when a new vector is created 
+            # with the same name "s" is reset
+  get <- function() { x } # Function that stores the matrix "x"
+  setsolve <- function(solve)  { s <<- solve } # Function that superassigns the value "s"
+  getsolve <- function() { s } # Function that returns "s"
+  
+  list(get = get,
+       setsolve = setsolve,
+       getsolve = getsolve) # Names of the functions, type in makeCacheMatrix() to see the three functions
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { # Create the function cacheSolve
+  s <- x$getsolve() # Calls the function getsolve and stores the value in "s"
+  if(!is.null(s)) { # If "s" is anything other than NULL
+    message("getting cached data") # Prints "getting cached data"
+    return(s) # Returns the solved matrix
+  }
+  thematrix <- x$get() # Otherwise use the get function to store the matrix in "thematrix"
+  s <- solve(thematrix) # Solve "thematrix"
+  x$setsolve(s) # Stores the calculation in function setsolve
+  print(s) # Prints the solved matrix, "s"
 }
+
+# Example test
+# newmat <- makeCacheMatrix(matrix(c(1,2,3,4), nrow = 2, ncol = 2))
+# cacheSolve(newmat)
+# then repeat to check if the calculation was stored
+# cacheSolve(newmat)
